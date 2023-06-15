35c4641c1b1935f7948222871fd29cf10ca98862
Kevin Minehart <kmineh0151@gmail.com>
2023-06-15 12:23:46 -0500

    add 2+2 test

--- main_test.go
+++ main_test.go
@@ -6,4 +6,7 @@ func TestAdd(t *testing.T) {
 	if res := Add(1, 2); res != 3 {
 		t.Fatalf("Add(1, 2) should equal '%d' but equals '%d'", 3, res)
 	}
+	if res := Add(2, 2); res != 4 {
+		t.Fatalf("Add(2, 2) should equal '%d' but equals '%d'", 3, res)
+	}
 }
